824ddf15817f75a59eaf8a95dc219f02e5e5465a
bac <cungbac.tran@gmail.com>
2019-06-26 23:38:17 +0700

    bac commit

--- baitap_ggplot/histogram.R
+++ baitap_ggplot/histogram.R
@@ -3,26 +3,8 @@
 library(ggplot2)
 
 #1. Nhu Y
-#-----------------HISTOGRAM----------------#
-## Basic ggplot2 histogram
-library(dplyr)
-library(ggplot2)
 
 
-data = data.frame(value=rnorm(10000))
-data
-head(data)
-ggplot(data,aes(x=value)) +
-  geom_histogram() #bins = 0.2
-ggplot(data,aes(x=value)) +
-  geom_histogram(binwidth = 0.1)
-
-##change color
-ggplot(data,aes(x=value)) +
-  geom_histogram(binwidth = 0.2, aes(fill = ..count..))
-ggplot(data,aes(x=value)) +
-  geom_histogram(binwidth = 0.2,col = "white",fill = rgb(0.7,0.2,0.2,0.4)) #rgb la ham cho phep tao mau theo cuong do tu 0 den 1
-?rgb
 
 
 #2. Thuy Hong
