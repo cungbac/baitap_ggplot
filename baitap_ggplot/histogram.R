bcf72363bccf8f8dbe68734c12f62d37a9e14a07
bac <cungbac.tran@gmail.com>
2019-06-26 21:23:37 +0700

    clear

--- baitap_ggplot/histogram.R
+++ baitap_ggplot/histogram.R
@@ -3,24 +3,18 @@
 library(ggplot2)
 
 #1. Nhu Y
-mtcars
-str(mtcars)
-ggplot(mtcars, aes(x = wt)) +
-  geom_histogram()
+
+
 
 
 #2. Thuy Hong
-mtcars
-str(mtcars)
-ggplot(mtcars, aes(x = wt)) +
-  geom_histogram()
+
+
 
 
 #3. Hong Lam
-iris
-str(iris)
-ggplot(iris,aes(x = Sepal.Length))+
-  geom_histogram(binwidth = 2)
+
+
 
 
 
