2861e0adb2d0a1652da219ce0109a9b382e287d4
bac <cungbac.tran@gmail.com>
2019-06-29 12:00:58 +0700

    clean

--- baitap_ggplot/histogram.R
+++ baitap_ggplot/histogram.R
@@ -4,28 +4,6 @@ library(ggplot2)
 
 #1. Nhu Y
 
-#-----------------HISTOGRAM----------------#
-## Basic ggplot2 histogram
-library(dplyr)
-library(ggplot2)
-
-
-data = data.frame(value=rnorm(10000))
-data
-head(data)
-ggplot(data,aes(x=value)) +
-  geom_histogram() #bins = 0.2
-ggplot(data,aes(x=value)) +
-  geom_histogram(binwidth = 0.1)
-
-##change color
-ggplot(data,aes(x=value)) +
-  geom_histogram(binwidth = 0.2, aes(fill = ..count..))
-ggplot(data,aes(x=value)) +
-  geom_histogram(binwidth = 0.2,col = "white",fill = rgb(0.7,0.2,0.2,0.4)) #rgb la ham cho phep tao mau theo cuong do tu 0 den 1
-?rgb
-
-
 
 
 #2. Thuy Hong
@@ -54,16 +32,6 @@ ggplot(data,aes(x=value)) +
 
 
 #7. Cung Bac
-ddata %>%
-  filter( price<300 ) %>%
-  ggplot( aes(x=price)) +
-  stat_bin(breaks=seq(0,300,10), fill="#69b3a2", color="#e9ecef", alpha=0.9) +
-  ggtitle("Night price distribution of Airbnb appartements") +
-  theme_ipsum() +
-  theme(
-    plot.title = element_text(size=12)
-  )
-
 
 
 
