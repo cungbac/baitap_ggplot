8edb353e47b94c972b7dded95a91aa52ab2c7bd0
unknown <phamminhly1997@gmail.com>
2019-06-29 15:52:48 +0700

    L day len 5

--- baitap_ggplot/histogram.R
+++ baitap_ggplot/histogram.R
@@ -927,8 +927,334 @@ mtext(c("Margin 1","Margin 2","Margin 3","Margin 4") , at=c(0.7,0.22,0.7,1.1) ,
 
 
 #5. Minh Ly
+### Boxplot on top of histogram ###
 
+library(ggplot2)
 
+# Creating data
+my_variable = c(rnorm(1000,0,2), rnorm(1000,9,2))
+
+# Layout to split the scream:
+layout(mat = matrix(c(1,2),2,1, byrow = TRUE), height = c(1,8))
+
+# Draw the boxplot and the histogram:
+par(mar=c(0,3.1,1.1,2.1))
+boxplot(my_variable, horizontal = TRUE, ylim=c(-10,20), xaxt='n',
+        col = rgb(0.8,0.8,0,0.5), frame = T)
+par(mar=c(4,3.1,1.1,2.1))
+hist(my_variable, breaks = 40, col=rgb(0.2,0.8,0.5,0.5), border=F, main = '',
+     xlab='value, of the variable', xlim=c(-10,20))
+
+### Two histograms with melt colors ###
+#Create data:
+Ixos = rnorm(4000,100,30)
+Primadur = rnorm(4000,300,20)
+
+#Represent separately first:
+par(mfrow=c(1,2))
+hist(Ixos, breaks = 30, xlim = c(0,300), col = rgb(1,0,0,0.5), xlab = 'height',
+     ylab='nbr of plants', main = '')
+hist(Ixos, breaks = 30, xlim = c(0,300), col=rgb(1,0,0,0.5), xlab = 'height',
+     ylab = 'nbr of plants', main = 'distribution of height of 2 durum wheat varieties')
+hist(Primadur, breaks = 30, xlim = c(0,300), col = rgb(0,0,1,0.5), add = T)
+legend('topright', legend = c('Ixos','Primadur'), col = c(rgb(1,0,0,0.5),
+                                                          rgb(0,0,1,0.5)),pt.cex=2,pch=15)
+
+### Mirrored Histogram ###
+
+# Create data:
+x1 = rnorm(100)
+x2 = rnorm(100) + rep(2,100)
+par(mfrow = c(2,1))
+
+# Make the plot:
+par(mar = c(0,5,3,3))
+hist(x1, main = '', xlim = c(-2,5), ylab = 'Frequency for x1', xlab = '',
+     ylim = c(0,25), xaxt = 'n', las = 1, col = 'slateblue1', breaks = 10)
+par(mar=c(5,5,0,3))
+hist(x2, main = '', xlim = c(-2,5), ylab = 'Frequency for x2', xlab = 
+       'value of my variable', ylim = c(25,0), las = 1, col = 'tomato3', breaks = 10)
+
+### Histogram with colored tail ###
+#Create data:
+my_variable=rnorm(2000,0,10)
+
+# Calculate histogram, but do not draw it:
+my_hist = hist(my_variable, breaks = 40, plot = F)
+
+# Color Vector:
+my_color = ifelse(my_hist$breaks < -10, rgb(0.2,0.8,0.5,0.5),
+                  ifelse(my_hist$breaks >=10, 'purple', rgb(0.2,0.2,0.2,0.2)))
+
+# Final plot:
+plot(my_hist, col = my_color, border = F, main = '', xlab = 'value of the variable',
+     xlim = c(-40,40))
+
+### Histogram without border
+
+# Create data:
+my_variable=c(rnorm(1000,0,2), rnorm(1000,9,2))
+
+# Draw the histogram with borded = F
+hist(my_variable, breaks = 40, col = rgb(0.2,0.8,0.5,0.5), border = F, main='')
+
+# Library:
+library(ggplot2)
+
+# Dataset:
+data = data.frame(value=rnorm(10000))
+
+# Basic histogram:
+ggplot(data, aes(x = value)) + geom_histogram()
+
+# Custom Binning. I can just give the size of the bin
+ggplot(data, aes(x = value)) + geom_histogram(binwidth = 0.05)
+
+# Uniform color:
+ggplot(data, aes(x=value)) + geom_histogram(binwidth = 0.2, color = 'white',
+                                            fill =rgb(0.2,0.7,0.1,0.4))
+
+# Proportional color
+ggplot(data, aes(x = value)) + geom_histogram(binwidth = 0.2, aes(fill = ..count..))
+
+# Library:
+library(ggplot2)
+library(gridExtra)
+
+# Crate data:
+set.seed(123)
+var = rnorm(1000)
+
+# Without theme:
+plot1 = qplot(var, fill = I(rgb(0.1,0.2,0.4,0.6)))
+
+# With themes:
+plot2 = plot1 + theme_bw() + annotate('text', x = -1.9,
+                                      y = 75, label = 'bw()', col = 'orange',
+                                      size = 4)
+plot3 = plot1 + theme_classic() + annotate('text', x = -1.9, y = 75,
+                                           col = 'orange', label = 'classic',
+                                           size = 4)
+plot4 = plot1+theme_gray()+annotate("text", x = -1.9, y = 75, label = "gray()" 
+                                    , col="orange" , size=4)
+plot5 = plot1+theme_linedraw()+annotate("text", x = -1.9, y = 75, 
+                                        label = "linedraw()" , col="orange" , size=4)
+plot6 = plot1+theme_dark()+annotate("text", x = -1.9, y = 75, 
+                                    label = "dark()" , col="orange" , size=4)
+plot7 = plot1+theme_get()+annotate("text", x = -1.9, y = 75, 
+                                   label = "get()" , col="orange" , size=4)
+plot8 = plot1+theme_minimal()+annotate("text", x = -1.9, y = 75, 
+                                       label = "minimal()" , col="orange" , size=4)
+
+# Arrange and display the plots into a 2x1 grid
+grid.arrange(plot1, plot2, plot3, plot4, ncol = 2)
+grid.arrange(plot5, plot6, plot7, plot8, ncol = 4)
+
+### Density plot with ggplot 2###
+# For the weather AUS data set:
+library(rattle)
+
+# To generate a density plot:
+library(ggplot2)
+cities = c('Canberra', 'Darwin', 'Melbourne', 'Sydney')
+ds = subset(weatherAUS, Location %in% cities & ! is.na(Temp3pm))
+p = ggplot(ds, aes(Temp3pm, colour = Location, fill = Location))
+p = p + geom_density(alpha = 0.55)
+p
+
+### Stacked density graph ###
+library(ggplot2)
+
+# Use diamonds dataset:
+data(diamonds)
+head(diamonds)
+
+# plot 1: Density of price for each type of cut of the diamond:
+ggplot(data = diamonds, aes(x = price, group = cut, fill = cut)) +
+  geom_density(adjust = 1.5)
+
+# Plot 2: Density plot with transparency (using the alpha argument):
+ggplot(data = diamonds, aes(x = price, group = cut, fill = cut)) +
+  geom_density(adjust = 1.5, alpha = 0.2)
+
+# Plot 3: Stacked density plot:
+ggplot(diamonds, aes(x = price, group = cut, fill = cut)) +
+  geom_density(adjust = 1.5, position = 'fill')
+
+# Plot 4: 
+ggplot(diamonds, aes(x = depth, y = ..density..)) +
+  geom_density(aes(fill = cut), position = 'stack') +
+  xlim(50,75) +
+  theme(legend.position = 'none')
+
+### Mirror plot:
+# Create data:
+x1 = rnorm(100)
+x2 = rnorm(100, mean = 2)
+par(mfrow = c(2,1))
+
+# Make the plot:
+par(mar=c(0,5,3,3))
+plot(density(x1), main = '', xlab = '', ylim = c(0,1), xaxt = 'n', las = 1,
+     col = 'slateblue1', lwd = 4)
+par(mar = c(5,5,0,3))
+plot(density(x2), main = '', xlab = 'value of my variable', ylim = c(1,0),
+     las = 1, col ='tomato3', lwd = 4)
+
+### Interactive 3D plot/plotly ###
+
+# Plotly library:
+library(plotly)
+
+# Let's use the volcano dataset:
+head(volcano)
+
+# 3D plot:
+p = plot_ly(z = volcano, type = 'surface')
+p
+
+### Basic interactive histogram/plotly:
+
+# Basic histogram of one vector only:
+graph1 = plot_ly(x = rnorm(500), type = 'historgram')
+graph1
+
+### Overlaid interactive histogram/plotly:
+
+# Overlaid histogram of 2 vectors:
+graph = plot_ly(x = rnorm(500), opacity = 0.6, type = 'historgram') %>%
+  add_trace(x = rnorm(500) + 1) %>%
+  layout(barmode = 'overlay')
+graph
+
+# Library:
+library(tidyverse)
+library(plotly)
+library(dplyr)
+library(ggplot2)
+
+# A classic histogram for the iris data set:
+ggplot(iris, aes(x=Sepal.Length)) + geom_histogram()
+
+# Transform a litte bit the dataset to make dots:
+don = iris %>%
+  arrange(Sepal.Length) %>%
+  mutate(var_rounded = (Sepal.Length+1) - ((Sepal.Length+1) %% 0.2) %>%
+           mutate(y = ave(var_rounded, var_rounded, FUN=seq_along))
+         
+         # Make the plot(middle):
+         ggplot(don, aes(x = var_rounded, y =y)) +
+           geom_point(size = 6, color = 'skyblue')
+         
+         # Improve the plot, make it interactive 
+         don = don %>%
+           mutate(text = paste('ID: ', rownames(iris), '\n',
+                               "Sepal Length: ", Sepal.Length, "\n", "Species:: ", Species, sep=""))
+         p = ggplot(don, aes(x = var_rounded, y = y)) +
+           geom_point(aes(text = text), size=6, color='skyblue') +
+           xlab('Sepal Length') +
+           ylab('# of individual') +
+           theme_classic() +
+           theme(legend.position = 'none', axis.line.y = element_blank(),
+                 axis.text = element_text(size=15))
+         p
+         
+         # Use the magic of gglotly to have an interactive version
+         ggplotly(p, tooltip='text')
+         
+         ### One title for 2 graphs:
+         
+         # Creating data:
+         Ixos = rnorm(4000, 100, 30)
+         Primadur = Ixos + rnorm(4000, 0, 30)
+         
+         # Divide the screen in 1 line and two columns:
+         par(mfrow = c(1,2), oma = c(0,0,2,0))
+         
+         # Make the margin around each graph a bit smaller:
+         par(mar = c(4,2,2,2))
+         
+         # Classical histogram and plot:
+         hist(Ixos, main = '', breaks = 30, col = rgb(0.3,0.5,1,0.4), xlab = 'hight', 
+              ylab = 'nbr of plants')
+         plot(Ixos, Primadur, main = '', pch = 20, cex = 0.4, col = rgb(0.3,0.5,1,0.4),
+              xlab = 'primadur', ylab = 'Ixos')
+         
+         # And I add only One title:
+         mtext('Primadur: Distribution and correlation with Ixos', outer = TRUE,
+               cex = 1.5, font = 4, col = rgb(0.1, 0.3, 0.5, 0.5))
+         
+         ### Set margin size with par mar function ###
+         # Set the margin size (huge margins):
+         par(mar = c(7,8,7,7))
+         
+         # Create data:
+         a = runif(29,0.4)
+         hist(a, border = F, col=rgb(0.2,0.2,0.8,0.7), main = '', ylab = '', xlab = '')
+         
+         # Add the text:
+         mtext(c('margin 1', 'margin 2', 'margin 3', 'margin 4'), at = c(0.7, 0.22, 0.7,1.1),
+               line = c(-25, -10, 2, -10), col = 'orange', cex = 1.5)
+         
+         ### Histogram:
+         par(bg = 'grey90')
+         par(mar=c(0,0,0,0))
+         a = rnorm(400, mean = -5, sd = 12)
+         b = rnorm(400, mean = 20, sd = 3)
+         c = rnorm(200, mean = -20, sd = 3)
+         hist(a, col = rgb(1,0.2,0.2,0.7), breaks = 50, xlim = c(-40, 40),
+              main = '', ylim = c(-20, 70))
+         hist(b, col = rgb(0,1,0.5,1), breaks = 15, add = T)
+         hist(c, col = rgb(0.6, 0.4, 1,1), breaks = 15, add = T)
+         hist(c, col = rgb(0.6,0.4,1,1), breaks = 15, add = T)
+         
+         ### Split Screen function ###
+         
+         # Create Data:
+         a = seq(1,29) + 4*runif(29,0.4)
+         b = seq(1,29)^2 + runif(29,0.98)
+         
+         # I divide the screen in 2 line and 1 column only:
+         my_screen_step1 = split.screen(c(2,1))
+         
+         # I add one graph on the screen number 1 which is on top:
+         screen(my_screen_step1[1])
+         plot(a,b, pch=20, xlab = 'value of a', cex = 3, col = rgb(0.4,0.9,0.8,0.5))
+         
+         # Divide the second creen in 2 columns:
+         my_screen_step2 = split.screen(c(1,2), screen = my_screen_step1[2])
+         screen(my_screen_step2[1])
+         hist(a, border = F, col = rgb(0.2,0.2,0.8,0.7), main = '', xlab = 'distribution of a')
+         screen(my_screen_step2[2])
+         hist(b, border = F, col = rgb(0.8,0.2,0.8,0.7), main = '', xlab = 'distribution of b')
+         
+         ### Split screen with layout###
+         
+         # Create random data:
+         a = seq(129, 1) + 4*runif(129, 0.4)
+         b = seq(1, 129)^2 + 4*runif(129, 0.98)
+         
+         nf = layout(matrix(c(1,1,2,3),2,2,byrow = T))
+         layout.show(nf)
+         
+         # add plots in each part of screen:
+         nf = layout(matrix(c(1,1,2,3), 2,2, byrow = TRUE
+                            hist(a, breaks = 30, border = F, col = rgb(0.1,0.8,0.3,0.5), xlab = 'distribution of a',
+                                 main = '')
+                            boxplot(a, xlab = 'a', col = rgb(0.8,0.8,0.3,0.5), las = 2)
+                            boxplot(b, xlab = 'b', col = rgb(0.4,0.2,0.3,0.5), las = 2)
+                            
+                            # Can also custom the size of each screen! let's make the right part smaller:
+                            # set the layout:
+                            nf = layout(matrix(c(1,1,2,3), 2, 2, byrow = T), c(3,1), c(2,2), TRUE)
+                            layout.show(nf)
+                            
+                            # Add the plots:
+                            hist(a, breaks = 30, bordr = F, col = rgb(0.1,0.8,0.3,0.5), xlab = 'distribution of a',
+                                 main = '')
+                            boxplot(a, xlab = 'a', col = rgb(0.8,0.8,0.3,0.5), las = 2)
+                            boxplot(b, xlab = 'b', col = rgb(0.4,0.2,0.3,0.5), las = 2)
+                            
 
 #6. Hong Phuc
 
